614fe5fa280f2a6f5dadfc7ef24748159b3ef879
Leul Zewdu <Leulzewduchanie@gmail.com>
2024-12-03 00:55:34 +0300

    finished input managment

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,24 @@ import Converter from './Converter'
 import './App.css'
 
 function App() {
+    const [f_unit,setFunit] = useState('');
+    
+    const [to_unit,setToUnit] = useState('');
+    
+    const [num,setNum] = useState(1.0);
 
+      console.log(f_unit);
+      console.log(to_unit);
   return (
     <>
-      <Converter/>
+      <Converter 
+        From_Unit={f_unit} 
+        setFrom_Unit={setFunit}
+        To_Unit={to_unit}
+        setToUnit={setToUnit}
+        Num={num}
+        setNum={setNum}
+        />
     </>
   )
 }
--- src/Converter.jsx
+++ src/Converter.jsx
@@ -1,21 +1,24 @@
-const Converter = () => {
+const Converter = ({Num,setNum,From_Unit,setFrom_Unit,To_Unit,setToUnit}) => {
+
+    
     return ( 
         <div>
             <h1>Temprature Converter</h1>
             <p>Enter the temprature ,select units and submits</p>
-            <input type="number" autoFocus></input>
-            <select name="fromUnit" id="fromUnit">
+            <input value={Num} onChange={(e)=>setNum(e.target.value)} type="number" autoFocus></input>
+            <select name="fromUnit" id="fromUnit" value={From_Unit} onChange={(e)=>setFrom_Unit(e.target.value)}>
                 <option value=''>From Unit</option>
                 <option value='Fahrenite'>Fahrenite</option>
                 <option value='Celsues'>Celsues</option>
                 <option value='Kelvin'>Kelvin</option>
             </select>
-            <select name="toUnit" id="toUnit">
+            <select name="toUnit" id="toUnit" value={To_Unit} onChange={(e)=>setToUnit(e.target.value)}>
                 <option value=''>To Unit</option>
                 <option value='Fahrenite'>Fahrenite</option>
                 <option value='Celseus'>Celsues</option>
                 <option value='Kelvin'>Kelvin</option>
             </select>
+            <button>Convert</button>
         </div>
      );
 }
